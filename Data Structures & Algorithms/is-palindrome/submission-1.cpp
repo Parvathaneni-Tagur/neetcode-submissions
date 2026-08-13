d136940e109bf4eb1371a03cfcf42f420c6ef48a
P. Tagur <89292665+Parvathaneni-Tagur@users.noreply.github.com>
2026-08-13 17:12:22 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+   public:
+    bool isPalindrome(string s) {
+        if (s.empty()) {
+            return true;
+        }
+        int start = 0, end = s.size() - 1;
+        while (start <= end) {
+            if (!isalnum(s[start])) {
+                start++;
+                continue;
+            } else if (!isalnum(s[end])) {
+                end--;
+                continue;
+            }
+            if (tolower(s[start]) != tolower(s[end])) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
